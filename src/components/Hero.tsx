4ccb8cbc201b1ed08f93ba4df8ca5f235e620722
Norman Yap <normanyaptw@yahoo.com>
2023-05-23 02:15:23 +1000

    change font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='w-full flex flex-col text-center justify-center mx-auto bg-gradient-to-r from-gray-900 to-gray-500 h-[570px] text-white'>
-        <div className="text-6xl p-10 font-bold">Touched By God</div>
-        <div className="text-6xl p-10 font-bold">Save Once</div>
-        <div className="text-6xl p-10 font-bold">Never Lost</div>
+        <div className="md:mt-20 md:text-6xl text-5xl p-10 font-bold  mt-[80px]">Touched By God</div>
+        <div className=" md:text-6xl text-5xl p-10 font-bold">Save Once</div>
+        <div className=" md:text-6xl text-5xl p-10 font-bold">Never Lost</div>
     </div>
   )
 }
